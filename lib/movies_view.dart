c507aa2390caa75e55f2c8c08e3d740a49eb9547
tihomir-mac <tihomir.trifonov.@sirma.bg>
2022-11-30 20:08:16 +0200

    show fetch movies with title, poster and imdbRating

--- lib/movies_view.dart
+++ lib/movies_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'movie_model.dart';
 import 'movies_page.dart';
 
 class MoviesView extends StatelessWidget {
@@ -13,7 +14,72 @@ class MoviesView extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Movies Application'),
         ),
-        body: Container()
+        body: Container(
+          color: Colors.blueGrey,
+          child: FutureBuilder<List<Movie>>(
+            future: state.futureMovie,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                List<Movie> movies = snapshot.data!;
+                return ListView.builder(
+                    itemCount: movies.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        margin: const EdgeInsets.all(8),
+                        elevation: 5,
+                        color: Colors.black12,
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Column(
+                            children: [
+
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: ClipRRect(
+                                  borderRadius: const BorderRadius.all(Radius.circular(8.0),
+                                  ),
+                                  child: Image.network(
+                                      movies[index].posterurl,
+                                      width: double.infinity,
+                                      // height: 150,
+                                      fit:BoxFit.fill
+
+                                  ),
+                                ),
+                              ),
+                              Text(movies[index].title,
+                                style: const TextStyle(
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white
+                                ),
+                              ),
+                              const SizedBox(height: 5,),
+                              Text(
+                                movies[index].imdbRating,
+                                style: const TextStyle(
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    });
+              }
+              if (snapshot.hasError) {
+                print(snapshot.error.toString());
+                return const Text('error');
+              }
+              return const CircularProgressIndicator();
+            },
+          ),
+        ),
     );
   }
 }
