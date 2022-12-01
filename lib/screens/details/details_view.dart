fdb145f36afb7d467caf9fbc81f56f84b56fb2b3
tihomir-mac <tihomir.trifonov.@sirma.bg>
2022-12-01 20:46:50 +0200

    add poster, title, rating and year in details screen

--- lib/screens/details/details_view.dart
+++ lib/screens/details/details_view.dart
@@ -16,8 +16,36 @@ class DetailsView extends StatelessWidget {
         title: const Text('Detail movie'),
       ),
       body: SingleChildScrollView(
-        child: text(text: state.movies.title),
+        child: detailsMovie(),
       ),
     );
   }
+
+  Widget detailsMovie() {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(child: posterImage(imageUrl: state.movies.posterurl)),
+          const SizedBox(height: 15),
+          text(text: state.movies.title),
+          const SizedBox(height: 8),
+          Row(
+            children: [
+              text(
+                text: state.movies.avrRating.toStringAsFixed(1),
+                size: 20,
+              ),
+              const SizedBox(width: 8),
+              ratingBar(state.movies.avrRating),
+            ],
+          ),
+          const SizedBox(height: 8),
+          text(text: 'Year: ${state.movies.year}', size: 20),
+        ],
+      ),
+    );
+  }
+
 }
