553033eae950dda134547a66fc5f3eb15a3bb283
tihomir-mac <tihomir.trifonov.@sirma.bg>
2022-12-01 20:25:03 +0200

    create details screen

---
+++ lib/screens/details/details_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class DetailsPage extends StatefulWidget {
+  const DetailsPage({Key? key}) : super(key: key);
+
+  @override
+  State<DetailsPage> createState() => _DetailsPageState();
+}
+
+class _DetailsPageState extends State<DetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
