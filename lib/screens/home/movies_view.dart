db9c3c4cea5f57621404c5a8319f077e16dfe9a6
tihomir-mac <tihomir.trifonov.@sirma.bg>
2022-12-08 18:09:35 +0200

    new widget with body of movies view

--- lib/screens/home/movies_view.dart
+++ lib/screens/home/movies_view.dart
@@ -32,48 +32,51 @@ class MoviesView extends StatelessWidget {
           ),
         ],
       ),
-      body: Container(
-        color: Colors.blueGrey,
-        child: FutureBuilder<List<Movie>>(
-          future: state.futureMovie,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              List<Movie> movies = snapshot.data!;
-              return ListView.builder(
-                itemCount: movies.length,
-                itemBuilder: (context, index) {
-                  return  movieCard(
-                      context: context,
-                      movie: movies[index],
-                      onPressedLikeButton: (Movie movie) {
-                        state.setMovies(movies[index]);
-                      },
-                      onTapMovie: (BuildContext context, Movie movie) {
-                        state.onTapMovie(context, movie);
-                      }
-                  );
-                },
-              );
-            }
-            if (snapshot.hasError) {
-              return Center(
-                child: Text(
-                  'Error: ${snapshot.error.toString()}',
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                  ),
-                ),
-              );
-            }
-            return const Center(child: CircularProgressIndicator(color: Colors.white,));
-          },
-        ),
-      ),
+      body: body(context),
     );
   }
 
+  Widget body(BuildContext context) {
+    return Container(
+      color: Colors.blueGrey,
+      child: FutureBuilder<List<Movie>>(
+        future: state.futureMovie,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List<Movie> movies = snapshot.data!;
+            return ListView.builder(
+              itemCount: movies.length,
+              itemBuilder: (context, index) {
+                return  movieCard(
+                    context: context,
+                    movie: movies[index],
+                    onPressedLikeButton: (Movie movie) {
+                      state.setMovies(movies[index]);
+                    },
+                    onTapMovie: (BuildContext context, Movie movie) {
+                      state.onTapMovie(context, movie);
+                    }
+                );
+              },
+            );
+          }
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(
+                'Error: ${snapshot.error.toString()}',
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
+              ),
+            );
+          }
+          return const Center(child: CircularProgressIndicator(color: Colors.white,));
+        },
+      ),
+    );
+  }
 
   void onTapMovie(BuildContext context, Movie movie) {
     Navigator.push(context,
